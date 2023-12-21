461d93177faa70e17ffee0b8f83eca136f8e55c2
Zoheb S <zohebs31@gmail.com>
2023-12-21 18:39:07 +0530

    jenkins-server-terraform

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app"
-    region = "us-east-1"
+    bucket = "zoheb-terraform"
+    region = "ap-south-1"
     key = "eks/terraform.tfstate"
   }
 }
